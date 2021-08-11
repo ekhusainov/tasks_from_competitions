ee966f2c7a1a5a8fbcbf485511345d0474a58b2b
ekhusainov <xluttiy@gmail.com>
2021-08-11 18:58:40 +0300

    plus function

--- yandex_cup_2021/task_d.py
+++ yandex_cup_2021/task_d.py
@@ -1,19 +1,43 @@
+from collections import defaultdict
 import sys
+from typing import List, Dict, Tuple
 
 
-def read_data():
+def read_data() -> Tuple[int, List[str]]:
     data = sys.stdin.read().split('\n')
     games_number = int(data[0])
     players = data[1:-1]
     return games_number, players
 
 
-def check_two_degree(number):
+def check_two_degree(number: int) -> bool:
     if number == 1:
         return True
     if number & 1:
         return False
     return check_two_degree(number >> 1)
+
+
+def building_dictionary_from_names(data_names: List[str]) -> Dict[str, int]:
+    name_frequency = defaultdict(int)
+    for current_two_names in data_names:
+        name1, name2 = current_two_names.split()
+        name_frequency[name1] += 1
+        name_frequency[name2] += 1
+    return name_frequency
+
+
+def data_and_dict_to_sorted_frequency_count(data_names: List[str],
+                                            name_frequency: Dict[str, int])\
+        -> List[Tuple[int]]:
+    name_frequency_data = []
+    for current_two_names in data_names:
+        name1, name2 = current_two_names.split()
+        freq1 = name_frequency[name1]
+        freq2 = name_frequency[name2]
+        name_frequency_data.append((min(freq1, freq2), max(freq1, freq2)))
+    name_frequency_data = sorted(name_frequency_data)
+    return name_frequency_data
 
 
 def main():
--- yandex_cup_2021/test_task_d.py
+++ yandex_cup_2021/test_task_d.py
@@ -2,7 +2,10 @@ import pytest
 
 from task_d import (
     check_two_degree,
+    building_dictionary_from_names,
+    data_and_dict_to_sorted_frequency_count,
 )
+
 
 @pytest.mark.parametrize(
     "number, etalon_answer",
@@ -22,4 +25,112 @@ def test_check_two_degree(number, etalon_answer):
     current_answer = check_two_degree(number)
     assert etalon_answer == current_answer, (
         f"{number} is not {current_answer}"
+    )
+
+
+@pytest.mark.parametrize(
+    "data, etalon_answer",
+    [
+        pytest.param(
+            [
+                "IVANOV PETROV",
+                "PETROV BOSHIROV",
+                "BOSHIROV IVANOV",
+            ],
+            {
+                "IVANOV": 2,
+                "PETROV": 2,
+                "BOSHIROV": 2,
+            },
+        ),
+        pytest.param(
+            [
+                "GORBOVSKII ABALKIN",
+                "SIKORSKI KAMMERER",
+                "SIKORSKI GORBOVSKII",
+                "BYKOV IURKOVSKII",
+                "PRIVALOV BYKOV",
+                "GORBOVSKII IURKOVSKII",
+                "IURKOVSKII KIVRIN",
+            ],
+            {
+                "GORBOVSKII": 3,
+                "ABALKIN": 1,
+                "SIKORSKI": 2,
+                "KAMMERER": 1,
+                "BYKOV": 2,
+                "IURKOVSKII": 3,
+                "PRIVALOV": 1,
+                "KIVRIN": 1,
+            }
+        ),
+    ]
+)
+def test_building_dictionary_from_names(data, etalon_answer):
+    current_answer = building_dictionary_from_names(data)
+    assert dict(current_answer) == etalon_answer, (
+        f"Wroing {data}"
+    )
+
+
+@pytest.mark.parametrize(
+    "data_names, name_frequency, etalon_answer",
+    [
+        pytest.param(
+            [
+                "IVANOV PETROV",
+                "PETROV BOSHIROV",
+                "BOSHIROV IVANOV",
+            ],
+            {
+                "IVANOV": 2,
+                "PETROV": 2,
+                "BOSHIROV": 2,
+            },
+            [
+                (2, 2),
+                (2, 2),
+                (2, 2),
+            ]
+        ),
+        pytest.param(
+            [
+                "GORBOVSKII ABALKIN",
+                "SIKORSKI KAMMERER",
+                "SIKORSKI GORBOVSKII",
+                "BYKOV IURKOVSKII",
+                "PRIVALOV BYKOV",
+                "GORBOVSKII IURKOVSKII",
+                "IURKOVSKII KIVRIN",
+            ],
+            {
+                "GORBOVSKII": 3,
+                "ABALKIN": 1,
+                "SIKORSKI": 2,
+                "KAMMERER": 1,
+                "BYKOV": 2,
+                "IURKOVSKII": 3,
+                "PRIVALOV": 1,
+                "KIVRIN": 1,
+            },
+            [
+                (1, 2),
+                (1, 2),
+                (1, 3),
+                (1, 3),
+                (2, 3),
+                (2, 3),
+                (3, 3),
+            ]
+        ),
+    ]
+)
+def test_data_and_dict_to_sorted_frequency_count(data_names, name_frequency, etalon_answer):
+    current_asnwer = data_and_dict_to_sorted_frequency_count(
+        data_names,
+        name_frequency,
+    )
+    current_asnwer = sorted(current_asnwer)
+    assert etalon_answer == current_asnwer, (
+        f"{data_names}, {name_frequency}"
     )
