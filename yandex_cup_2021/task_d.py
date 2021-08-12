737ae433f4a39d4b34189e1a73e966bc26d1ada5
ekhusainov <xluttiy@gmail.com>
2021-08-12 23:37:49 +0300

    fail 23 test

--- yandex_cup_2021/task_d.py
+++ yandex_cup_2021/task_d.py
@@ -11,12 +11,12 @@ def read_data() -> Tuple[int, List[str]]:
     return games_number, players
 
 
-def check_two_degree(number: int) -> bool:
-    if number == 1:
-        return True
-    if number & 1:
-        return False
-    return check_two_degree(number >> 1)
+# def check_two_degree(number: int) -> bool:
+#     if number == 1:
+#         return True
+#     if number & 1:
+#         return False
+#     return check_two_degree(number >> 1)
 
 
 def building_dictionary_from_names(data_names: List[str]) -> Dict[str, int]:
@@ -32,19 +32,35 @@ def list_strings_to_list_list_strings(data_names):
     return [i.split() for i in data_names]
 
 
+def check_if_the_number_of_participants_is_correct(data_list_list_string, players_number):
+    full_list_players = []
+    for i in data_list_list_string:
+        full_list_players.append(i[0])
+        full_list_players.append(i[1])
+    full_list_players = list(set(full_list_players))
+    return len(full_list_players) == players_number
+
+
 def check_to_correct(data_list_list_string, data_dict, begin_stage):
+    if not check_if_the_number_of_participants_is_correct(data_list_list_string, begin_stage * 2):
+        return False
     current_arr = data_list_list_string.copy()
     power_2 = int(log2(begin_stage))
     for stage_number in range(1, power_2 + 1):
         number_of_delete = 0
         list_for_delete = []
         for name1, name2 in current_arr:
+            if data_dict[name1] == stage_number and data_dict[name2] == stage_number:
+                return False
             if data_dict[name1] == stage_number or data_dict[name2] == stage_number:
                 list_for_delete.append([name1, name2])
                 number_of_delete += 1
 
         if number_of_delete != begin_stage:
             return False
+        if stage_number == 1:
+            if not check_if_the_number_of_participants_is_correct(list_for_delete, begin_stage * 2):
+                return False
         for name1, name2 in list_for_delete:
             current_arr.remove([name1, name2])
         begin_stage = begin_stage // 2
@@ -55,64 +71,43 @@ def check_to_correct(data_list_list_string, data_dict, begin_stage):
     return False
 
 
-def data_and_dict_to_sorted_frequency_count(data_names: List[str],
-                                            name_frequency: Dict[str, int])\
-        -> List[Tuple[int]]:
-    name_frequency_data = []
-    for current_two_names in data_names:
-        name1, name2 = current_two_names.split()
-        freq1 = name_frequency[name1]
-        freq2 = name_frequency[name2]
-        name_frequency_data.append((min(freq1, freq2), max(freq1, freq2)))
-    name_frequency_data = sorted(name_frequency_data)
-    return name_frequency_data
+# def data_and_dict_to_sorted_frequency_count(data_names: List[str],
+#                                             name_frequency: Dict[str, int])\
+#         -> List[Tuple[int]]:
+#     name_frequency_data = []
+#     for current_two_names in data_names:
+#         name1, name2 = current_two_names.split()
+#         freq1 = name_frequency[name1]
+#         freq2 = name_frequency[name2]
+#         name_frequency_data.append((min(freq1, freq2), max(freq1, freq2)))
+#     name_frequency_data = sorted(name_frequency_data)
+#     return name_frequency_data
 
 
-def number_of_players_to_olympic_couple(number: int) -> List[Tuple[int]]:
-    range_list = list(range(1, 2 ** number + 1))
-    pairs = []
-    for i in range(number):
-        current_pairs = list(zip(range_list[0::2], range_list[1::2]))
-        for j in current_pairs:
-            pairs.append(j)
-        range_list = [i[0] for i in current_pairs]
-    return pairs
+# def number_of_players_to_olympic_couple(number: int) -> List[Tuple[int]]:
+#     range_list = list(range(1, 2 ** number + 1))
+#     pairs = []
+#     for i in range(number):
+#         current_pairs = list(zip(range_list[0::2], range_list[1::2]))
+#         for j in current_pairs:
+#             pairs.append(j)
+#         range_list = [i[0] for i in current_pairs]
+#     return pairs
 
 
-def list_tuple_to_list_str(pairs):
-    return [str(i[0]) + " " + str(i[1]) for i in pairs]
+# def list_tuple_to_list_str(pairs):
+#     return [str(i[0]) + " " + str(i[1]) for i in pairs]
 
 
-def check_data_for_double_name(data_names):
-    for current_two_names in data_names:
-        name1, name2 = current_two_names.split()
-        if name1 == name2:
-            return True
-    return False
+# def check_data_for_double_name(data_names):
+#     for current_two_names in data_names:
+#         name1, name2 = current_two_names.split()
+#         if name1 == name2:
+#             return True
+#     return False
 
 
 def main():
-    # data_names = [
-    #     "9 11",
-    #     "5 6",
-    #     "1 9",
-    #     "9 10",
-    #     "15 16",
-    #     "13 15",
-    #     "3 4",
-    #     "1 2",
-    #     "7 8",
-    #     "1 3",
-    #     "13 14",
-    #     "1 5",
-    #     "9 13",
-    #     "11 12",
-    #     "5 7",
-    # ]
-    # data_list_list  = list_strings_to_list_list_strings(data_names)
-    # data_dict = building_dictionary_from_names(data_names)
-    # ans = check_to_correct(data_list_list, data_dict, 8)
-    # print(ans)
     games_number, players = read_data()
     begin_stage = (games_number + 1) // 2
     data_list_list = list_strings_to_list_list_strings(players)
@@ -122,27 +117,6 @@ def main():
         print("NO SOLUTION")
     else:
         print(*ans[0])
-    # power_2 = int(log2(games_number + 1))
-    # pairs = number_of_players_to_olympic_couple(power_2)
-    # generatic_players = list_tuple_to_list_str(pairs)
-    # etalon_answer = data_and_dict_to_sorted_frequency_count(
-    #     generatic_players,
-    #     building_dictionary_from_names(generatic_players)
-    # )
-    # dict_playes = building_dictionary_from_names(players)
-    # current_answer = data_and_dict_to_sorted_frequency_count(
-    #     players,
-    #     dict_playes,
-    # )
-    # if etalon_answer == current_answer:
-    #     list_dict_playes = dict_playes.items()
-    #     answer = [i[0] for i in list_dict_playes if i[1] == power_2]
-    #     if check_data_for_double_name(players):
-    #         print("NO SOLUTION")
-    #     else:
-    #         print(*answer)
-    # else:
-    #     print("NO SOLUTION")
 
 
 if __name__ == "__main__":
