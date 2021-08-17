08d49bd619253ac1996390745cdeeb9fc6a36895
ekhusainov <xluttiy@gmail.com>
2021-08-17 21:53:23 +0300

    Update task_d.py

--- yandex_cup_2021/task_d.py
+++ yandex_cup_2021/task_d.py
@@ -11,14 +11,6 @@ def read_data() -> Tuple[int, List[str]]:
     return games_number, players
 
 
-# def check_two_degree(number: int) -> bool:
-#     if number == 1:
-#         return True
-#     if number & 1:
-#         return False
-#     return check_two_degree(number >> 1)
-
-
 def building_dictionary_from_names(data_names: List[str]) -> Dict[str, int]:
     name_frequency = defaultdict(int)
     for current_two_names in data_names:
@@ -69,42 +61,6 @@ def check_to_correct(data_list_list_string, data_dict, begin_stage):
     if data_dict[name1] == stage_number or data_dict[name2] == stage_number:
         return current_arr
     return False
-
-
-# def data_and_dict_to_sorted_frequency_count(data_names: List[str],
-#                                             name_frequency: Dict[str, int])\
-#         -> List[Tuple[int]]:
-#     name_frequency_data = []
-#     for current_two_names in data_names:
-#         name1, name2 = current_two_names.split()
-#         freq1 = name_frequency[name1]
-#         freq2 = name_frequency[name2]
-#         name_frequency_data.append((min(freq1, freq2), max(freq1, freq2)))
-#     name_frequency_data = sorted(name_frequency_data)
-#     return name_frequency_data
-
-
-# def number_of_players_to_olympic_couple(number: int) -> List[Tuple[int]]:
-#     range_list = list(range(1, 2 ** number + 1))
-#     pairs = []
-#     for i in range(number):
-#         current_pairs = list(zip(range_list[0::2], range_list[1::2]))
-#         for j in current_pairs:
-#             pairs.append(j)
-#         range_list = [i[0] for i in current_pairs]
-#     return pairs
-
-
-# def list_tuple_to_list_str(pairs):
-#     return [str(i[0]) + " " + str(i[1]) for i in pairs]
-
-
-# def check_data_for_double_name(data_names):
-#     for current_two_names in data_names:
-#         name1, name2 = current_two_names.split()
-#         if name1 == name2:
-#             return True
-#     return False
 
 
 def main():
